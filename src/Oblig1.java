57807838655c6f03da7e90cb65e3250ec50d72ab
hussain1810 <Haugerud1810>
2018-09-03 14:14:43 +0200

    oppgave 1 fullført

--- src/Oblig1.java
+++ src/Oblig1.java
@@ -1,7 +1,23 @@
 public class Oblig1 {
     public static void main (String[] args) {
-        System.out.println("Test");
-        System.out.println(1+2);
-        System.out.println("Hei");
+        int[] a = {5, 3, 1, 10, 4};
+        System.out.println("Største tall: " + maks(a));
+    }
+
+    public static int maks(int[] a)
+    {
+        if (a.length < 0) {
+            throw new IllegalArgumentException("Tabellen er tom!");
+        }
+
+        for (int i = 0; i < a.length-1; i++) {
+            if (a[i] > a[i+1]) {
+                int temp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
+            }
+        }
+
+        return a[a.length-1];
     }
 }
